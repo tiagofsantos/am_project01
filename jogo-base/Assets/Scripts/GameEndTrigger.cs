9b45573d2d6fc60c7d8faf04c728f99b889baed8
tiagofsantos <tiago.afsantos@hotmail.com>
2018-07-17 22:24:22 +0100

    Adicionei um trigger para o jogo acabar

---
+++ jogo-base/Assets/Scripts/GameEndTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            player.GetComponent<PlayerController>().enabled = false;
+            player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            player.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+
+
+            //end screen e isso
+        }
+    }
+
+}
